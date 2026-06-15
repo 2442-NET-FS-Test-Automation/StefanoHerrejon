e4d3b0110223c99d8b3557b5342213a6d985c4c3
Stefano H.A. <stefano.herrejon@hotmail.com>
2026-06-15 13:34:33 -0600

    Se añadio el add product, y correccion de errores

--- week1/AsyncLab1/AsyncLab1.App/Program.cs
+++ week1/AsyncLab1/AsyncLab1.App/Program.cs
@@ -10,7 +10,7 @@ public class Program
 
 
         //We create a list of products, hardcoded
-        List < Product > products = new List<Product>(); //We create an empty list of products
+        List < Product > products = new List<Product>(); //We create an empty list of products and we add 2 products
         Food food1 = new Food("Apples", 10.99, 11,  new DateOnly(2026, 12, 1));
         Cloths cloths1 = new Cloths("T-Shirt", 15.75, 3, 'M');
 
@@ -73,21 +73,95 @@ public class Program
 
     public static void NoOption()
     {
-        Console.WriteLine("No valid option detected");
+        Console.WriteLine("No valid option detected. Only enter numerical values from 1 - 5.");
     }
 
     public static void NoOption(int option)
     {
-        Console.WriteLine($"No valid option detected for {option}");
+        Console.WriteLine($" {option} is not a valid option");
     }
 
     public static void AddProduct(List<Product> products)
     {
         Console.WriteLine("Option 1: Add New Product");
-        //Que producto, food or cloths?
-        //console input detalles
-        //Crete new product
-        //Añadir a la lista
+        Console.WriteLine("What product do you want to add? \n1) Food \n2) Clothes");
+
+        int opcion = 0;
+        try
+        {
+            opcion = Convert.ToInt32(Console.ReadLine());
+        }catch(Exception e)
+        {
+            Console.WriteLine("Option not available");
+            Console.WriteLine(e.Message);
+            opcion = 0;
+        }
+
+        string name = "";
+        double price = 0.0;
+        int units = 0;
+        
+
+        if(opcion == 1)
+        {
+            DateOnly date = new DateOnly();
+            string dateNoFormat = "";
+            try
+            {
+                
+                Console.WriteLine("Add new Food option"); //string nombre, double precio, int unidades, DateOnly expirationDate
+                Console.WriteLine("Name:");
+                name = Console.ReadLine();
+                Console.WriteLine("Price:");
+                price = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Units:");
+                units = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Expiration Date, format: dd-MM-yyyy");
+                dateNoFormat = Console.ReadLine();
+                date = DateOnly.Parse(dateNoFormat);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Wrong format exeption");
+            }
+            products.Add(new Food(name, price, units, date));
+            Console.WriteLine($"New Food option {name} added");
+            
+
+
+            
+        }else if(opcion == 2)
+        {
+            char size = 'A';
+            string a;
+            Console.WriteLine("Add new Cloth piece"); //string nombre, double precio, int unidades, char size
+            try
+            {
+                
+                Console.WriteLine("Add new Food option"); //string nombre, double precio, int unidades, DateOnly expirationDate
+                Console.WriteLine("Name:");
+                name = Console.ReadLine();
+                Console.WriteLine("Price:");
+                price = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Units:");
+                units = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Size: S,M,L");
+                a = Console.ReadLine();
+                size = a[0];
+
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Wrong format exeption");
+            }
+            products.Add(new Cloths(name, price, units, size));
+            Console.WriteLine($"New Cloth option {name} added");
+            
+        }
+        else
+        {
+            Console.WriteLine("Option not available");
+        }
 
     }
 
@@ -96,7 +170,7 @@ public class Program
         int numProduct;
         int newUnits = 0;
         Console.WriteLine("Option 2: Restock a Product");
-        Console.WriteLine("Which product do you want to retock? Input in number of product.");
+        Console.WriteLine("Which product do you want to restock? Input in number of product.");
         ListProducts(products);
 
         try
@@ -202,7 +276,7 @@ public class Program
 
     public static void ListProducts(List<Product> list)
     {
-        Console.WriteLine("Option 4: List of all Products");
+        Console.WriteLine("Option 4: List all Products");
         int x = 0;
         for(int i = 0; i < list.Count; i++)
         {
