313b4dde799418abb84ef6b067d34bd111553b80
Stefano <stefano.herrejon@hotmail.com>
2026-07-07 11:40:33 -0600

    Reinicio de proyecto

--- LibraryKata/LibraryKata.App/obj/Debug/net10.0/LibraryKata.App.AssemblyInfo.cs
+++ LibraryKata/LibraryKata.App/obj/Debug/net10.0/LibraryKata.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryKata.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae78d58f32b68aa2c5e7f501bfa18896503c5d93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2dc1e86fcafa97270309216a856cb4f254f0d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryKata.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryKata.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
